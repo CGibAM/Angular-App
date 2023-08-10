d10e4b9b0026f87c0172596891c5abd9a6e11c0d
Cristian Gibran <magaaa511@gmail.com>
2023-08-10 12:40:17 -0600

    Creation of Project

---
+++ AppAPI/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using AppAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppAPI.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
+
---
+++ AppAPI/Program.cs
@@ -0,0 +1,26 @@
+
+using AppAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaulConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
